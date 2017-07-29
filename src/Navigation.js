7f82a01cbee5430f407a5b5d31a6360c6d058535
d-ashokkumar <ashokkumar01cbe@gmail.com>
2017-07-29 19:23:13 +0530

    changes related to header in app

--- src/Navigation.js
+++ src/Navigation.js
@@ -4,9 +4,47 @@ class Navigate extends Component{
     render(){
         return(
             <div style={{position: 'relative'}}>
-
-        <Header title="Expense Manager">
-        </Header>
+                 
+{
+        <header title="Expense Manager" style={{display: "-ms-flexbox",
+    display: "flex",
+   "msFlexDirection": "column",
+   "flexDirection": "column",
+    "msFlexWrap": "nowrap",
+    "flexWrap": "nowrap",
+    "msFlexPack": "start",
+    "justifyContent": "flex-start",
+    "WebkitBoxSizing": "border-box",
+    "boxSizing": "border-box",
+    "msFlexNegative": 0,
+    "flexShrink": 0,
+    width: "100%",
+    margin: 0,
+    padding: 0,
+    border: "none",
+    "minHeight": "64px",
+    "maxheight": "1000px",
+    "zIndex": 3,
+   "backgroundColor": "#3f51b5",
+    color: "#fff",
+    "WebkitBoxShadow": "0 2px 2px 0 rgba(0,0,0,.14), 0 3px 1px -2px rgba(0,0,0,.2), 0 1px 5px 0 rgba(0,0,0,.12)",
+    "boxShadow": "0 2px 2px 0 rgba(0,0,0,.14), 0 3px 1px -2px rgba(0,0,0,.2), 0 1px 5px 0 rgba(0,0,0,.12)",
+  "WebkitTransitionDuration": ".2s",
+   "OTransitionDuration":".2s",
+    "transitionDuration": ".2s",
+    "WebkitTransitionTimingFunction": "cubic-bezier(.4,0,.2,1)",
+    "OTransitionTimingFunction": "cubic-bezier(.4,0,.2,1)",
+    "transitionTimingFunction": "cubic-bezier(.4,0,.2,1)",
+   "WebkitTransitionProperty": "max-height"
+ 
+   
+   }}><span style={{  "fontFamily":" Roboto,Helvetica,Arial,sans-serif",
+    "fontSize": "20px",
+   "lineHeight": 1,
+   "paddingTop":"30px",
+    "letterSpacing": ".02em",
+    "fontWeight": 400,}}>Expense Manager</span>
+        </header>}
 
 </div>
         )
